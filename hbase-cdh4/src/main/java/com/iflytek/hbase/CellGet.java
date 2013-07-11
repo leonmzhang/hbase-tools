11e9ca11df39554605d29f2e51e7a6c55621b97d
mingzhang2 <mingzhang2@iflytek.com>
2013-07-11 18:43:27 +0800

    m

--- hbase-cdh4/src/main/java/com/iflytek/hbase/CellGet.java
+++ hbase-cdh4/src/main/java/com/iflytek/hbase/CellGet.java
@@ -1,5 +1,7 @@
 package com.iflytek.hbase;
 
+import java.util.Properties;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.cli.Options;
@@ -11,22 +13,56 @@ import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.log4j.PropertyConfigurator;
 
 class CellGet {
   public static final Log LOG = LogFactory.getLog(CellGet.class);
-  public static final String ZK_QUORUM = "mirage-pro.hbase0001.gz.voicecloud.cn"; 
+  public static final String ZK_QUORUM = "mirage-pro.hbase0001.gz.voicecloud.cn";
+  
+  public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+  public static final String TAB = "\t";
+  
+  public static void initLogProperties() {
+    Properties properties = new Properties();
+    properties.setProperty("log4j.rootLogger", "DEBUG, CONSOLE");
+    properties.setProperty("log4j.appender.CONSOLE",
+        "org.apache.log4j.ConsoleAppender");
+    properties.setProperty("log4j.appender.CONSOLE.layout",
+        "org.apache.log4j.PatternLayout");
+    properties.setProperty("log4j.appender.CONSOLE.layout.ConversionPattern",
+        "%d{yyyy-MM-dd HH:mm:ss} [%5p] %C %M %t:%L:%x - %m%n");
+    PropertyConfigurator.configure(properties);
+  }
+  
+  public static void initCmdOptions() {
+    Options options = new Options();
+    options.addOption("n", "number", true, "The number of scan count!");
+    options.addOption("", "", true, "");
+    options.addOption("", "", true, "");
+  }
+  
+  public static void usage() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Usage: hbase-tools [--config confdir] COMMAND");
+    sb.append(LINE_SEPARATOR);
+    sb.append("where command is one of:");
+    sb.append(LINE_SEPARATOR);
+    sb.append(TAB);
+    sb.append("scan");
+    System.out.println(sb.toString());
+  }
   
   public static void main(String[] args) throws Exception {
-	Configuration conf = new Configuration();
-	conf.set("hbase.zookeeper.quorum", ZK_QUORUM);
-    HTable table = new HTable(conf, "personal_");
-    //Get get = new Get(Bytes.toBytes("201000000"));
-    //get.addColumn(Bytes.toBytes("p"), Bytes.toBytes("contact.bin"));
-    Scan scan = new Scan();
-    //Result result = table.get(get);
-    ResultScanner scanner = table.getScanner(scan);
-    Result result = scanner.next();
+    initLogProperties();
+    if(args.length == 0) {
+      usage();
+      System.exit(1);
+    }
+    LOG.info("tool start!");
     
-    table.close();
+    
+    System.out.println("");
+    
+    LOG.info("tool end");
   }
 }
