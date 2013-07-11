15a11d2417f720c99dbea61e65502692313a0b63
mingzhang2 <mingzhang2@iflytek.com>
2013-07-11 09:41:34 +0800

    m

--- hbase-cdh4/src/main/java/com/iflytek/hbase/Get.java
+++ hbase-cdh4/src/main/java/com/iflytek/hbase/Get.java
@@ -1,8 +1,14 @@
 package com.iflytek.hbase;
 
-//import org.apache.
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLineParser;
 
 class Get {
-    //private 
-
+  public static final Log LOG = LogFactory.getLog(Get.class);
+  
+  public static void main(String[] args) {
+    
+  }
 }
