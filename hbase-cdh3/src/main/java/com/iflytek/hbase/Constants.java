a0dd65b1ceceac38da0bb29cf6525e1464da712a
mingzhang2 <mingzhang2@iflytek.com>
2013-07-16 13:19:45 +0800

    m

---
+++ hbase-cdh3/src/main/java/com/iflytek/hbase/Constants.java
@@ -0,0 +1,12 @@
+package com.iflytek.hbase;
+
+public class Constants {
+  public static final String LINE_SEPARATOR = System
+      .getProperty("line.separator");
+  public static final String TAB = "\t";
+  public static final String DOUBLE_TAB = "\t\t";
+  public static final String TRIPLE_TAB = "\t\t\t";
+  
+  public static final String SCAN_MIN_DATE = "scan.min.date";
+  public static final String SCAN_MAX_DATE = "scan.max.date";
+}
--- hbase-cdh3/src/main/java/com/iflytek/hbase/SyncTable.java
+++ hbase-cdh3/src/main/java/com/iflytek/hbase/SyncTable.java
@@ -1,8 +1,16 @@
 package com.iflytek.hbase;
 
+import java.io.File;
+import java.io.PrintWriter;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -27,6 +35,29 @@ public class SyncTable implements Tool {
   @Override
   public int run(String[] args) throws Exception {
     Scan scan = new Scan();
+    String minDateStr = conf.get(Constants.SCAN_MIN_DATE);
+    String maxDateStr = conf.get(Constants.SCAN_MAX_DATE);
+    long minStamp = Common.dateStrToUnixTimestamp(minDateStr);
+    long maxStamp = Common.dateStrToUnixTimestamp(maxDateStr);
+    scan.setTimeRange(minStamp, maxStamp);
+    scan.addFamily(Bytes.toBytes("cf"));
+    
+    HTable table = new HTable("personal");
+    ResultScanner scanner = table.getScanner(scan);
+    Result result = null;
+    
+    StringBuilder sb = new StringBuilder();
+    while((result = scanner.next()) != null) {
+      sb.append(Bytes.toString(result.getRow()));
+      sb.append(Constants.LINE_SEPARATOR);
+    }
+    
+    File output = new File("output");
+    PrintWriter pw = new PrintWriter(output);
+    pw.write(sb.toString());
+    pw.close();
+    scanner.close();
+    table.close();
     return 0;
   }
   
