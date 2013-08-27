41e60c9c3a083b97a1fec89f5d029698251c8c13
mingzhang2 <mingzhang2@iflytek.com>
2013-08-27 12:37:52 +0800

    m

--- hbase-cdh3/src/main/java/com/iflytek/hbase/main/CheckAppData.java
+++ hbase-cdh3/src/main/java/com/iflytek/hbase/main/CheckAppData.java
@@ -2,7 +2,9 @@ package com.iflytek.hbase.main;
 
 import java.io.DataInput;
 import java.io.DataOutput;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -22,6 +24,9 @@ import com.iflytek.hbase.Common;
 
 public class CheckAppData {
   private static Log LOG = LogFactory.getLog(CheckAppData.class);
+  
+  private static String LINE_SEPARATOR = System.getProperty("line.separator");
+  
   private HTablePool tablePool;
   
   public class AppDataFilter implements Filter {
@@ -93,16 +98,20 @@ public class CheckAppData {
   }
   
   public int runTool(Configuration conf, String[] args) {
+    
     tablePool = new HTablePool(conf, 128);
     HTableInterface table = tablePool.getTable("personal");
     
     Scan scan = new Scan();
     AppDataFilter filter = new AppDataFilter();
-    scan.setFilter(filter);
+    // scan.setFilter(filter);
     scan.addFamily(Bytes.toBytes("cf"));
     
     ResultScanner scanner = null;
     try {
+      File output = new File("gz_list");
+      PrintWriter pw = new PrintWriter(output);
+      
       scanner = table.getScanner(scan);
       
       Result result = null;
@@ -129,6 +138,8 @@ public class CheckAppData {
         rowKey = Bytes.toString(result.getRow());
         scanLastRow = rowKey;
         LOG.info(rowKey);
+        pw.write(rowKey + LINE_SEPARATOR);
+        pw.flush();
         
       } while (result != null);
     } catch (IOException e) {
