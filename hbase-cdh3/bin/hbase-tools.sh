65ca6fceeaa3c884de527debdda115735f496f6d
mingzhang2 <mingzhang2@iflytek.com>
2013-08-28 17:11:34 +0800

    m

--- hbase-cdh3/bin/hbase-tools.sh
+++ hbase-cdh3/bin/hbase-tools.sh
@@ -18,7 +18,11 @@ fi
 CLASSPATH=${CLASSPATH}:${bin}/../hbase-tools-cdh3-0.1.0-SNAPSHOT-jar-with-dependencies.jar:${bin}/../conf
 JAVA=${JAVA_HOME}/bin/java
 
-JVM_OPTS=" -Xdebug -Xrunjdwp:transport=dt_socket,suspend=y,server=y,address=3389 "
+DEBUG_FLAG=$1
+if [ ${DEBUG_FLAG} = "debug" ]; then
+  JVM_OPTS=" -Xdebug -Xrunjdwp:transport=dt_socket,suspend=y,server=y,address=3389 "
+  shift
+fi
 
 COMMAND=$1
 shift
