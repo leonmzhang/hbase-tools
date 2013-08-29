780d46bd26820391c15e374c22755e55ad464287
mingzhang2 <mingzhang2@iflytek.com>
2013-08-29 20:41:36 +0800

    m

--- hbase-cdh3/bin/check_personal_data.sh
+++ hbase-cdh3/bin/check_personal_data.sh
@@ -7,6 +7,8 @@ JAVA=${JAVA_HOME}/bin/java
 
 SUSPEND=y
 
+CLASSPATH=${CLASSPATH}:${bin}/../hbase-tools-cdh3-0.1.0-SNAPSHOT-jar-with-dependencies.jar:${bin}/../conf
+
 JVM_OPTS=" -Dbase.dir=${bin}/../"
 
 JVM_OPTS="${JVM_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,suspend=${SUSPEND},server=y,address=3389 "
