81f60ba51752eb5b91a1ac98262b32fa8a2e3484
mingzhang2 <mingzhang2@iflytek.com>
2013-08-29 13:41:07 +0800

    m

---
+++ hbase-cdh3/src/main/java/com/iflytek/hbase/main/PersonalSync.java
@@ -0,0 +1,13 @@
+package com.iflytek.hbase.main;
+
+public class PersonalSync {
+  
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
