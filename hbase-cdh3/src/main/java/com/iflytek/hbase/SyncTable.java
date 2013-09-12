346245793f5ae40ebb3ffe7018e7baae381da89f
mingzhang2 <mingzhang2@iflytek.com>
2013-09-12 09:36:58 +0800

    m

--- hbase-cdh3/src/main/java/com/iflytek/hbase/SyncTable.java
+++ hbase-cdh3/src/main/java/com/iflytek/hbase/SyncTable.java
@@ -324,7 +324,7 @@ public class SyncTable implements Tool {
                   newColumnByte, attributes);
               if (cellList.isEmpty()
                   || cellList.get(0).timestamp < oldTimestamp) {
-                LOG.info(firstSyncFlag ? "first sync, " : ""
+                LOG.info((firstSyncFlag ? "first sync, " : "")
                     + "sync cell, table: " + newTable + ", row: " + newRowKey
                     + ", column: " + newColumn + ", modify time: "
                     + Common.unixTimestampToDateStr(oldTimestamp));
